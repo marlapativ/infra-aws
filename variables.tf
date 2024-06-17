487732675eac18839cc47b1b931b9d0e485d4ab1
Marlapati Venkata Naga Sai Teja[marlapativ] <marlapati.v@northeastern.edu>
2024-06-17 13:01:37 -0400

    refactor: updating eks cluster defaults

--- variables.tf
+++ variables.tf
@@ -63,23 +63,34 @@ variable "network_acl_egress" {
 variable "eks_cluster" {
   type = object({
     name                    = string
-    version                 = string
-    ami_type                = string
+    version                 = optional(string, "1.29")
+    ip_family               = optional(string, "ipv4")
+    ami_type                = optional(string, "AL2_x86_64")
     authentication_mode     = optional(string, "API_AND_CONFIG_MAP")
     endpoint_public_access  = optional(bool, true)
     endpoint_private_access = optional(bool, true)
+    log_types = optional(list(string), [
+      "api",
+      "audit",
+      "authenticator",
+      "controllerManager",
+      "scheduler"
+    ])
     node_groups = list(object({
       name           = string
       ami_type       = optional(string)
       capacity_type  = optional(string, "ON_DEMAND")
       instance_types = optional(list(string), ["c3.large"])
+      desired_size   = optional(number, 3)
       min_size       = optional(number, 3)
       max_size       = optional(number, 6)
-      desired_size   = optional(number, 6)
-      update_config  = optional(object({
+      update_config = optional(object({
         max_unavailable = optional(number, 1)
-        max_surge       = optional(number, null)
-      }))
+        max_surge       = optional(number, 0)
+        }), {
+        max_unavailable = 1,
+        max_surge       = 0
+      })
     }))
   })
 }
