a8535da64f937f1040547fb9e5f1e73ce77dcd82
Marlapati Venkata Naga Sai Teja[marlapativ] <marlapati.v@northeastern.edu>
2024-06-26 21:57:08 -0400

    feat: kafka helm defaults

--- eks-bootstrap-kafka.tf
+++ eks-bootstrap-kafka.tf
@@ -1,9 +1,21 @@
+locals {
+  kafka_values_files = [for file_path in var.eks_bootstrap_kafka.values_file_paths : "${file(file_path)}"]
+}
+
 resource "kubernetes_namespace" "kafka" {
   provider = kubernetes
   metadata {
     name = var.eks_bootstrap_kafka.namespace
   }
   depends_on = [module.eks]
+}
+
+resource "random_password" "kafka_password" {
+  length  = 24
+  special = false
+  upper   = true
+  numeric = true
+  lower   = true
 }
 
 resource "helm_release" "kafka" {
@@ -14,6 +26,8 @@ resource "helm_release" "kafka" {
   chart      = var.eks_bootstrap_kafka.chart
   namespace  = kubernetes_namespace.kafka.metadata.0.name
 
+  values = local.kafka_values_files
+
   dynamic "set" {
     for_each = var.eks_bootstrap_kafka.values
     content {
@@ -22,5 +36,25 @@ resource "helm_release" "kafka" {
     }
   }
 
-  depends_on = [kubernetes_namespace.kafka, module.eks.cluster_name]
+  set {
+    name  = "global.storageClass"
+    value = kubernetes_storage_class.ebs.metadata.0.name
+  }
+
+  set_list {
+    name  = "sasl.client.users"
+    value = [var.eks_bootstrap_kafka_sensitive_values.username]
+  }
+
+  set_list {
+    name  = "sasl.client.passwords"
+    value = [random_password.kafka_password.result]
+  }
+
+  depends_on = [
+    kubernetes_namespace.kafka, 
+    kubernetes_storage_class.ebs,
+    random_password.kafka_password,
+    module.eks.cluster_name
+  ]
 }
